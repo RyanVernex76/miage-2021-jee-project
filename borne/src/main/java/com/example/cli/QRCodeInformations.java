18f84668ad886f0515aa31effe10860b6a52056d
Betul <betul45@hotmail.fr>
2021-12-17 16:42:49 +0100

    init Borne

---
+++ borne/src/main/java/com/example/cli/QRCodeInformations.java
@@ -0,0 +1,2 @@
+package com.example.cli;public class QRCode {
+}
--- greencab/src/main/java/fr/pantheonsorbonne/ufr27/miage/dao/CustomerDAO.java
+++ greencab/src/main/java/fr/pantheonsorbonne/ufr27/miage/dao/CustomerDAO.java
@@ -3,8 +3,8 @@ package fr.pantheonsorbonne.ufr27.miage.dao;
 import fr.pantheonsorbonne.ufr27.miage.exception.CustomerNotFoundException;
 import fr.pantheonsorbonne.ufr27.miage.model.Customer;
 
-public interface CustomerDAO {
+public interface PassengerDAO {
     Customer findMatchingCustomer(String email) throws CustomerNotFoundException;
 
-    Customer createNewCustomer(String fname, String lname, String email);
+    Customer createNewPassenger(String fname, String lname, String email);
 }
