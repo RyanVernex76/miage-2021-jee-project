c7ed8fa251a57ec9778ecf552d8d4f2d5df18d10
RyanVernex76 <ryanvernex@yahoo.fr>
2022-02-08 00:11:11 +0100

    reload component onclick take in charge

--- greencab/src/main/portfolio/src/app/car-recharge/car-recharge.component.ts
+++ greencab/src/main/portfolio/src/app/car-recharge/car-recharge.component.ts
@@ -2,10 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {Car} from "../../model/Car";
 import {GreenCabService} from "../green-cab.service";
 import {firstValueFrom, Observable} from "rxjs";
-import {addWarning} from "@angular-devkit/build-angular/src/utils/webpack-diagnostics";
-import {Recharge} from "../../model/Recharge";
 import {LoginService} from "../login.service";
-import {Juicer} from "../../model/Juicer";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-car-recharge',
@@ -17,7 +15,7 @@ export class CarRechargeComponent implements OnInit {
   @Input()
   cars: Array<Car> | undefined;
 
-  constructor(private greenCabService: GreenCabService, private loginService: LoginService) { }
+  constructor(private router: Router, private greenCabService: GreenCabService, private loginService: LoginService) { }
 
   ngOnInit() {
   }
@@ -26,7 +24,14 @@ export class CarRechargeComponent implements OnInit {
     if (this.loginService.currentUser != undefined) {
       await firstValueFrom(this.greenCabService.initRecharge(carId,
         this.loginService.currentUser.id));
+      this.reloadComponent();
     }
+  }
 
+  reloadComponent() {
+    let currentUrl = this.router.url;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([currentUrl]);
   }
 }
